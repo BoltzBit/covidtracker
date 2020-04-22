e79f21720632a8f07be2134431c8d4579751f3f5
BoltzBit <mateus_desktop@outlook.com>
2020-04-22 18:20:26 -0300

    project

--- src/api/index.js
+++ src/api/index.js
@@ -4,10 +4,45 @@ const url = 'https://covid19.mathdro.id/api';
 
 export const fetchData = async () => {
     try{
-        const response = await axios.get(url);
+        const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
 
-        return response;
+        const modifiedData = {
+            confirmed,
+            recovered,
+            deaths,
+            lastUpdate,
+        }
+
+        return modifiedData;
+
     }catch(err){
+        console.log(err);
+    }
+}
 
+export const fetchDailyData = async () => {
+    try{
+
+        const { data } = await axios.get(`${url}/daily`);
+        const modifiedData = data.map((dailyData) => ({
+            confirmed: dailyData.confirmed.total,
+            deaths: dailyData.deaths.total,
+            date: dailyData.reportDate,
+        }));
+
+        return modifiedData;
+    }catch(err){
+        console.log(err);
+    }
+}
+
+export const countries = async () => {
+    try{
+        const response = await axios.get(`${url}/countries`);
+
+        console.log(response);
+
+    }catch(err){
+        console.log(err);
     }
 }
--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { NativeSelect, FormControl }from '@material-ui/core';
+
+import { countries } from '../../api';
+import styles from './CountryPicker.module.css';
+
 
 const CountryPicker = () => {
+    const [ fetchedCountries, setFetchedCountries ] = useState([]);
+
+    useEffect(() => {
+        const fetchedCountries = async () => {
+            setFetchedCountries(await countries);
+        }
+
+        fetchedCountries();
+    },[setFetchedCountries]);
+
     return(
-        <h1>CountryPicker</h1>
+        <FormControl className={styles.formControl}>
+            <NativeSelect>
+                <option value="global">Global</option>
+            </NativeSelect>
+        </FormControl>
     );
 }
 
